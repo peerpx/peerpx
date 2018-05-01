8014cea85988d8a2d8b52be0beaacca6b919101d
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2018-05-01 18:20:15 +0200

    improve mocked datastore

--- core/datastore_mocked.go
+++ core/datastore_mocked.go
@@ -1,24 +1,30 @@
 package core
 
 // DatastoreMocked fake datastore for testing
-type DatastoreMocked struct{}
+type DatastoreMocked struct {
+	responseErr  error
+	responseData []byte
+}
 
 // NewDatastoreMocked return a fake datastore for testing puupose
-func NewDatastoreMocked() Datastore {
-	return &DatastoreMocked{}
+func NewDatastoreMocked(data []byte, err error) Datastore {
+	return &DatastoreMocked{
+		responseData: data,
+		responseErr:  err,
+	}
 }
 
 // Put implements datastore.Put
 func (d *DatastoreMocked) Put(key string, value []byte) error {
-	return nil
+	return d.responseErr
 }
 
 // Get implements datastore.Get
 func (d *DatastoreMocked) Get(key string) (data []byte, err error) {
-	return
+	return d.responseData, d.responseErr
 }
 
 // Delete implements datastore.Delete
 func (d *DatastoreMocked) Delete(key string) error {
-	return nil
+	return d.responseErr
 }

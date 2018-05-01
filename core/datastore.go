cc03e1f1665e4ae4d87a1da8382892f7bf47c095
Stéphane Depierrepont aka Toorop <toorop@toorop.fr>
2018-05-01 10:19:03 +0200

    datastore interface

---
+++ core/datastore.go
@@ -0,0 +1,18 @@
+package core
+
+import "errors"
+
+// Datastore represents the storage interface
+type Datastore interface {
+	// Put store value identified by key
+	Put(key string, value []byte) error
+
+	// Get return value associated with key
+	Get(key string) (value []byte, err error)
+
+	// Delete deletes value for a given key
+	Delete(key string) error
+}
+
+// ErrNotFoundInDatastore is returned by Get, Delete if key is not found
+var ErrNotFoundInDatastore = errors.New("datastore: key not found")

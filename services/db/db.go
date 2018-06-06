737bdcc4a9ba1587a9dd24f545105218cc9c08b7
Stéphane Depierrepont <toorop@gmail.com>
2018-06-06 17:29:43 +0200

    add sqlx wrapper

--- services/db/db.go
+++ services/db/db.go
@@ -1,33 +1,207 @@
 package db
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mssql"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+	"context"
+	"errors"
+
+	"database/sql"
+
+	"github.com/jmoiron/sqlx"
 )
 
-// DB is the database connector
-var DB *gorm.DB
+var (
+	// ErrNotInitialized (db == nil)
+	ErrNotInitialized = errors.New("db: service not initialized")
+)
 
-// InitDB initialize database connector
-// todo: get options/type from config
-func InitDB() (err error) {
-	DB, err = gorm.Open("sqlite3", "peerpx.db")
+var db *sqlx.DB
+
+// InitDatabase init database handler
+func InitDatabase(driverName, dataSourceName string) (err error) {
+	db, err = sqlx.Open(driverName, dataSourceName)
 	return err
 }
 
-// InitMockedDB is for tests
-func InitMockedDB(dsn string) sqlmock.Sqlmock {
-	_, mock, err := sqlmock.NewWithDSN(dsn)
-	if err != nil {
-		panic(err)
+func BeginTxx(ctx context.Context, opts *sql.TxOptions) (*sqlx.Tx, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
 	}
-	DB, err = gorm.Open("sqlmock", dsn)
-	if err != nil {
-		panic(err)
+	return db.BeginTxx(ctx, opts)
+}
+
+func Beginx() (*sqlx.Tx, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
 	}
-	return mock
+	return db.Beginx()
+}
+
+func BindNamed(query string, arg interface{}) (string, []interface{}, error) {
+	if db == nil {
+		return "", nil, ErrNotInitialized
+	}
+	return db.BindNamed(query, arg)
+}
+
+func DriverName() string {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.DriverName()
+}
+
+func Get(dest interface{}, query string, args ...interface{}) error {
+	if db == nil {
+		return ErrNotInitialized
+	}
+	return db.Get(dest, query, args)
+}
+
+func GetContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
+	if db == nil {
+		return ErrNotInitialized
+	}
+	return db.GetContext(ctx, dest, query, args)
+}
+
+func MapperFunc(mf func(string) string) {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	db.Mapper(mf)
+}
+
+func MustBegin() *sqlx.Tx {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.MustBegin()
+}
+
+func MustBeginTx(ctx context.Context, opts *sql.TxOptions) *sqlx.Tx {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.MustBeginTx(ctx, opts)
+}
+func MustExec(query string, args ...interface{}) sql.Result {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.MustExec(query, args)
+}
+
+func MustExecContext(ctx context.Context, query string, args ...interface{}) sql.Result {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.MustExecContext(ctx, query, args)
+}
+
+func NamedExec(query string, arg interface{}) (sql.Result, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.NamedExec(query, arg)
+}
+
+func NamedExecContext(ctx context.Context, query string, arg interface{}) (sql.Result, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.NamedExecContext(ctx, query, arg)
+}
+
+func NamedQuery(query string, arg interface{}) (*sqlx.Rows, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.NamedQuery(query, arg)
+}
+
+func NamedQueryContext(ctx context.Context, query string, arg interface{}) (*sqlx.Rows, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.NamedQueryContext(ctx, query, arg)
+}
+
+func PrepareNamed(query string) (*sqlx.NamedStmt, error) {
+	if db == nil {
+		return err, ErrNotInitialized
+	}
+	return db.PrepareNamed(query)
+}
+
+func PrepareNamedContext(ctx context.Context, query string) (*sqlx.NamedStmt, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.PrepareNamedContext(ctx, query)
+}
+
+func Preparex(query string) (*sqlx.Stmt, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.Preparex(query)
+}
+
+func PreparexContext(ctx context.Context, query string) (*sqlx.Stmt, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.PreparexContext(ctx, query)
+}
+
+func QueryRowx(query string, args ...interface{}) *sqlx.Row {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.QueryRowx(query, args)
+}
+func QueryRowxContext(ctx context.Context, query string, args ...interface{}) *sqlx.Row {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.QueryRowxContext(ctx, query, args)
+}
+func Queryx(query string, args ...interface{}) (*sqlx.Rows, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.Queryx(query, args)
+}
+
+func QueryxContext(ctx context.Context, query string, args ...interface{}) (*sqlx.Rows, error) {
+	if db == nil {
+		return nil, ErrNotInitialized
+	}
+	return db.QueryxContext(ctx, query, args)
+}
+
+func Rebind(query string) string {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.Rebind(query)
+}
+func Select(dest interface{}, query string, args ...interface{}) error {
+	if db == nil {
+		return ErrNotInitialized
+	}
+	return db.Select(dest, query, args)
+}
+
+func SelectContext(ctx context.Context, dest interface{}, query string, args ...interface{}) error {
+	if db == nil {
+		return ErrNotInitialized
+	}
+	return db.SelectContext(ctx, dest, query, args)
+}
+func Unsafe() *sqlx.DB {
+	if db == nil {
+		panic(ErrNotInitialized)
+	}
+	return db.Unsafe()
 }

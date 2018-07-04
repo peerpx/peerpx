ab7a27a44d401566936ab8d5541b9c885c64632a
Stéphane Depierrepont <toorop@gmail.com>
2018-07-04 17:31:14 +0200

    webfinger tests

--- cmd/server/handlers/webfinder_test.go
+++ cmd/server/handlers/webfinder_test.go
@@ -8,6 +8,10 @@ import (
 
 	"bytes"
 
+	"database/sql"
+
+	"fmt"
+
 	"github.com/labstack/echo"
 	"github.com/peerpx/peerpx/services/config"
 	"github.com/peerpx/peerpx/services/db"
@@ -16,12 +20,73 @@ import (
 )
 
 func TestWebfinger(t *testing.T) {
+	//init
 	config.InitBasicConfig(bytes.NewBuffer([]byte{}))
 	config.Set("hostname", "peerpx.social")
 	e := echo.New()
-	req := httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=acct:toorop@peerpx.social", nil)
+
+	// no resource
+	req := httptest.NewRequest(echo.GET, "/.well-know/webfinger", nil)
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+	}
+
+	// bad syntax
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=accttoorop@peerpx.social,foobar", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+	}
+
+	// no acct
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=act:toorop@peerpx.social,foo:bar", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+	}
+
+	// not addr
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=acct:tooroppeerpx.social", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+	}
+
+	// not local
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=acct:toorop@peerpx.org", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusNotFound, rec.Code)
+	}
+
+	// no such user
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=acct:toorop@peerpx.social", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	db.Mock.ExpectQuery("^SELECT(.*)").WillReturnError(sql.ErrNoRows)
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusNotFound, rec.Code)
+	}
+
+	// DB failure
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=acct:toorop@peerpx.social", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
+	db.Mock.ExpectQuery("^SELECT(.*)").WillReturnError(fmt.Errorf("boooooo!"))
+	if assert.NoError(t, Webfinger(c)) {
+		assert.Equal(t, http.StatusInternalServerError, rec.Code)
+	}
+
+	// ok
+	req = httptest.NewRequest(echo.GET, "/.well-know/webfinger?resource=acct:toorop@peerpx.social", nil)
+	rec = httptest.NewRecorder()
+	c = e.NewContext(req, rec)
 	row := sqlmock.NewRows([]string{"id", "username", "email", "password"}).AddRow(1, "john", "john@doe.com", "$2y$10$vjxV/XuyPaPuINLopc49COmFfxEiVFac4m0L7GgqvJ.KAQcfpmvCa")
 	db.Mock.ExpectQuery("^SELECT(.*)").WillReturnRows(row)
 
